60a382a4d778241b83f234d44ecc93ee7fed9040
henryjason <henryjason2009@hotmail.com>
2014-02-24 11:06:07 -0600

    Creacion de menu en page layout

---
+++ Proyecto WebI/Controllers/CRecomentacionController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Proyecto_WebI.Models;
+
+namespace Proyecto_WebI.Controllers
+{
+    public class CRecomentacionController : Controller
+    {
+        private DBRecomentaciones db = new DBRecomentaciones();
+
+        //
+        // GET: /CRecomentacion/
+
+        public ActionResult Index()
+        {
+            return View(db.oRecomendacion.ToList());
+        }
+
+        //
+        // GET: /CRecomentacion/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Recomentacion recomentacion = db.oRecomendacion.Find(id);
+            if (recomentacion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(recomentacion);
+        }
+
+        //
+        // GET: /CRecomentacion/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /CRecomentacion/Create
+
+        [HttpPost]
+        public ActionResult Create(Recomentacion recomentacion)
+        {
+            if (ModelState.IsValid)
+            {
+                db.oRecomendacion.Add(recomentacion);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(recomentacion);
+        }
+
+        //
+        // GET: /CRecomentacion/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Recomentacion recomentacion = db.oRecomendacion.Find(id);
+            if (recomentacion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(recomentacion);
+        }
+
+        //
+        // POST: /CRecomentacion/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Recomentacion recomentacion)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(recomentacion).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(recomentacion);
+        }
+
+        //
+        // GET: /CRecomentacion/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Recomentacion recomentacion = db.oRecomendacion.Find(id);
+            if (recomentacion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(recomentacion);
+        }
+
+        //
+        // POST: /CRecomentacion/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Recomentacion recomentacion = db.oRecomendacion.Find(id);
+            db.oRecomendacion.Remove(recomentacion);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Proyecto WebI/Models/Recomentacion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto_WebI.Models
+{
+    public class Recomentacion
+    {
+        [Required]
+        public Int32 ID { get; set; }
+        [Required]
+        public String Name { get; set; }
+        public String Télefono { get; set; }
+        [Required]
+        public String Email { get; set; }
+        public String Comentario { get; set; }
+       
+    }
+
+    public class DBRecomentaciones : DbContext
+    {
+        public DbSet<Recomentacion> oRecomendacion { get; set; }
+    }
+
+}
