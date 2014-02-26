78ed9aaabf2b8c467ce63f5505b386b0e36ffeab
henryjason <henryjason2009@hotmail.com>
2014-02-26 10:59:39 -0600

    configuracion email finalizado

--- Proyecto WebI/Controllers/HomeController.cs
+++ Proyecto WebI/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Proyecto_WebI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -14,6 +16,43 @@ namespace Proyecto_WebI.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Email()
+        {
+            return View();
+        }
+
+        public ActionResult SendMail(string Subject, string Body)
+        {
+
+
+            EMail oMail = new EMail();
+            EMail.Send("henryjason2009@hotmail.com", Subject, Body );
+
+            return Content("Success");
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Sendlink(FormCollection formCollection)
+        {
+            try
+            {
+
+                var mailMessage = new MailMessage();
+                mailMessage.To.Add("henryjason2009@hotmail.com");
+                mailMessage.Subject = "como estas";
+                mailMessage.Body = "como";
+                mailMessage.IsBodyHtml = true;
+
+                var smtpClient = new SmtpClient { EnableSsl = true };
+                smtpClient.Send(mailMessage);
+            }
+            catch
+            {
+
+            }
+            return null;
         }
 
     }
---
+++ Proyecto WebI/Models/EMail.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Xml;
+
+namespace Proyecto_WebI.Models
+{
+    public class EMail
+    {
+
+        private string FromAddress;
+        private string strToAddress;
+        private string strSmtpClient;
+        private string UserID;
+        private string Password;
+        private string ReplyTo;
+        private string SMTPPort;
+        private Boolean bEnableSSL;
+
+        private void InitMail()
+        {
+            FromAddress = System.Configuration.ConfigurationManager.AppSettings.Get("FromAddress");
+            strToAddress = System.Configuration.ConfigurationManager.AppSettings.Get("ToAddress");
+            //strSmtpClient = System.Configuration.ConfigurationManager.AppSettings.Get("SmtpClient");
+            strSmtpClient = "smtp.live.com";
+            UserID = System.Configuration.ConfigurationManager.AppSettings.Get("UserID");
+            Password = System.Configuration.ConfigurationManager.AppSettings.Get("Password");
+            ReplyTo = System.Configuration.ConfigurationManager.AppSettings.Get("ReplyTo");
+            SMTPPort = System.Configuration.ConfigurationManager.AppSettings.Get("SMTPPort");
+            if (System.Configuration.ConfigurationManager.AppSettings.Get("EnableSSL").ToUpper() == "YES")
+            {
+                bEnableSSL = true;
+            }
+            else
+            {
+                bEnableSSL = false;
+            }
+        }
+
+        public static void Send(string toAddress, string subject, string body)
+        {
+            var mailMessage = new MailMessage();
+            mailMessage.To.Add(toAddress);
+            mailMessage.Subject = subject;
+            mailMessage.Body = body;
+            mailMessage.IsBodyHtml = true;
+
+            var smtpClient = new SmtpClient { EnableSsl = true };
+            smtpClient.Send(mailMessage);
+        }
+
+    }
+
+
+}
